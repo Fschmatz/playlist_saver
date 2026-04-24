29876c4c804f82865df1060c5d47102d11ac9607
Fernando <fschmatz@hotmail.com>
2026-04-23 21:36:16 -0300

    2.4.0

---
+++ lib/service/app_parameter_service.dart
@@ -0,0 +1,55 @@
+import '../db/app_parameter_dao.dart';
+import '../class/app_parameter.dart';
+import 'store_service.dart';
+
+class AppParameterService extends StoreService {
+  final dbParams = AppParameterDAO.instance;
+
+  Future<void> saveParameter(AppParameter parameter) async {
+    await dbParams.insertOrUpdate(parameter.toMap());
+    await loadAppParameters();
+  }
+
+  Future<void> deleteParameter(String key) async {
+    await dbParams.delete(key);
+    await loadAppParameters();
+  }
+
+  Future<List<AppParameter>> getAll() async {
+    var resp = await dbParams.queryAllRows();
+    
+    return resp.isNotEmpty
+        ? resp.map((map) => AppParameter.fromMap(map)).toList()
+        : [];
+  }
+
+  Future<void> saveLastBackupDate() async {
+    DateTime now = DateTime.now();
+    String formattedDate = "${now.day.toString().padLeft(2, '0')}/${now.month.toString().padLeft(2, '0')}/${now.year} ${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}";
+    
+    await saveParameter(AppParameter(
+      key: 'lastBackupDate',
+      value: formattedDate,
+    ));
+  }
+
+  Future<String?> getLastBackupDate() async {
+    var resp = await dbParams.queryByKey('lastBackupDate');
+    return resp != null ? AppParameter.fromMap(resp).getValue() : null;
+  }
+
+  Future<List<Map<String, dynamic>>> loadAllParameters() {
+    return dbParams.queryAllRows();
+  }
+
+  Future<void> deleteAllParameters() async {
+    await dbParams.deleteAll();
+  }
+
+  Future<void> insertParametersFromRestoreBackup(List<dynamic> jsonData) async {
+    for (var item in jsonData) {
+      await dbParams.insertOrUpdate(item as Map<String, dynamic>);
+    }
+    await loadAppParameters();
+  }
+}
